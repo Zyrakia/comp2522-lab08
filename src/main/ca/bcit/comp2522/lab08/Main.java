7237427a30a169b10d334c25cf7b54ca7d449101
Zyrakia <ole.lammers@pm.me>
2024-10-30 21:48:25 -0700

    feat: added first filter

--- src/main/ca/bcit/comp2522/lab08/Main.java
+++ src/main/ca/bcit/comp2522/lab08/Main.java
@@ -1,20 +1,59 @@
 package ca.bcit.comp2522.lab08;
 
+import ca.bcit.comp2522.lab08.filter.CountriesProcessor;
+import ca.bcit.comp2522.lab08.filter.LongCountryNames;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The driver class for Lab 8 of COMP2522.
  *
- * @author Ole Lammers
+ * @author Ole Lammers, Tianyou Xie
  * @version 1.0
  */
 public class Main {
+
+    public static final CountriesProcessor[] FILTERS = {
+            new LongCountryNames() };
 
     /**
      * Entry point for the driver class.
      *
      * @param args the command line arguments
      */
-    public static void main(final String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(final String[] args) throws IOException {
+        final List<String> countries;
+        countries = Files.readAllLines(Paths.get("countries.txt"));
+
+        final Path matchesDirectoryPath;
+        matchesDirectoryPath = Paths.get("matches");
+        if (Files.notExists(matchesDirectoryPath)) {
+            Files.createDirectory(matchesDirectoryPath);
+        }
+
+        final Path outputFilePath;
+        outputFilePath = matchesDirectoryPath.resolve("data.txt");
+        if (Files.notExists(outputFilePath)) {
+            Files.createFile(outputFilePath);
+        }
+
+        for (final CountriesProcessor filter : Main.FILTERS) {
+            final List<String> linesToWrite;
+            linesToWrite = new ArrayList<>();
+
+            linesToWrite.add(filter.getTitle() + ":");
+            linesToWrite.addAll(filter.process(countries));
+            linesToWrite.add("");
+
+            Files.write(outputFilePath, linesToWrite,
+                        StandardOpenOption.APPEND);
+        }
     }
 
 }
---
+++ src/main/ca/bcit/comp2522/lab08/filter/CountriesProcessor.java
@@ -0,0 +1,60 @@
+package ca.bcit.comp2522.lab08.filter;
+
+import java.util.List;
+
+/**
+ * Represents a processor that can operate on a list of country names..
+ *
+ * @author Ole Lammers, Tianyou Xie
+ * @version 1.0
+ */
+public abstract class CountriesProcessor {
+
+    private final String title;
+
+    /**
+     * Creates a new processor.
+     *
+     * @param title the title of the processor
+     */
+    public CountriesProcessor(final String title) {
+        CountriesProcessor.validateTitle(title);
+        this.title = title;
+    }
+
+    /**
+     * Validates the given title to ensure it is within limits.
+     *
+     * @param title the title
+     */
+    private static void validateTitle(final String title) {
+        if (title == null || title.isBlank()) {
+            throw new IllegalArgumentException(
+                    "Processor title cannot be empty or null.");
+        }
+
+        if (title.contains(System.lineSeparator())) {
+            throw new IllegalArgumentException(
+                    "Processor title must be a single line.");
+        }
+    }
+
+    /**
+     * Takes in a list of country names and reduces or processes them into a
+     * list of strings that represent the processor result.
+     *
+     * @param countries the list of countries
+     * @return the list of strings representing the result
+     */
+    public abstract List<String> process(List<String> countries);
+
+    /**
+     * Returns the title of this processor.
+     *
+     * @return the title
+     */
+    public final String getTitle() {
+        return this.title;
+    }
+
+}
