7acd5f4c1373c0458a805221fc1477dd3b3711da
Zyrakia <ole.lammers@pm.me>
2024-10-30 20:47:05 -0700

    feat: initial setup

---
+++ src/main/ca/bcit/comp2522/lab08/Main.java
@@ -0,0 +1,20 @@
+package ca.bcit.comp2522.lab08;
+
+/**
+ * The driver class for Lab 8 of COMP2522.
+ *
+ * @author Ole Lammers
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Entry point for the driver class.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
